3fae31b19dfbf3bd00196b4e6cc3552e35df8ab5
Mann-Git <mannprajapti2502@gmail.com>
2025-07-03 01:39:39 -0700

    the code is updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,29 @@ import './App.css';
 function App() {
   return (
     <>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">MyApp</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav ms-auto">
+        <li className="nav-item">
+          <a className="nav-link active" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/features">Features</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
     <h1>Hello my name is Mann</h1>
+
+
     </>
   );
 }
